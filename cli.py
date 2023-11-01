d90642cd75343840fb0f76b55f608f6a4f2453da
Benjamin Good <good.benjamin@gmail.com>
2023-11-01 17:31:58 +0000

    Adding options to the query command to take in custom queries and contexts (#5)

--- cli.py
+++ cli.py
@@ -4,33 +4,29 @@ from vertexai.language_models import CodeChatModel, ChatModel
 from google.cloud import aiplatform
 from gcloudai.commands import cmd
 
-def send_message(prompt):
-    #aiplatform.init(project='crg-scratch-022423',)
-    code_chat_model = CodeChatModel.from_pretrained("codechat-bison@001")
-    code_chat_model = CodeChatModel.from_pretrained("codechat-bison@001")
+def send_message(prompt, context):
     code_chat_model = ChatModel.from_pretrained("chat-bison")
-    # with open('hello.py', 'r') as file:
-    #     context = file.read()
-    ignore_dirs = ["__pycache__", ".venv", ".vscode"]
     
-    context = ""
-    for root, dirs, files in os.walk('gcloudai'):
-        print(".")
-        # Remove directories in the ignore list from traversal
-        dirs[:] = [d for d in dirs if d not in ignore_dirs]
+    if context == "":
+        ignore_dirs = ["__pycache__", ".venv", ".vscode"]
         
-        for file in files:
-            filepath = os.path.join(root, file)
-            try:
-                with open(filepath, 'r', encoding='utf-8') as f:
-                    context += f.read() + "\n\n"  # Separate file contents with two newlines
-            except UnicodeDecodeError:
-                print(f"Skipped file due to encoding error: {filepath}")
+        for root, dirs, files in os.walk('gcloudai'):
+            print(".")
+            # Remove directories in the ignore list from traversal
+            dirs[:] = [d for d in dirs if d not in ignore_dirs]
+            
+            for file in files:
+                filepath = os.path.join(root, file)
+                try:
+                    with open(filepath, 'r', encoding='utf-8') as f:
+                        context += f.read() + "\n\n"  # Separate file contents with two newlines
+                except UnicodeDecodeError:
+                    print(f"Skipped file due to encoding error: {filepath}")
       
     chat = code_chat_model.start_chat(context=context)
     
     response = chat.send_message(prompt)
-    return response.text
+    return response.text 
 
 @click.group()
 def cli():
@@ -41,9 +37,11 @@ def ai():
     click.echo('Initialized AI')
 
 @click.command()
-def query():
-    click.echo('Calling Chat')
-    response = send_message("Provide a summary of this source code")
+@click.option('-q', '--query', required=False, type=string, default="Provide a summary of this source code")
+@click.option('-c', '--context', required=False, type=string, default="")
+def query(q, c):
+    click.echo('Calling Chat with Context')
+    response = send_message_with_context(q, c)
     click.echo(response)
 
 cli.add_command(ai)
