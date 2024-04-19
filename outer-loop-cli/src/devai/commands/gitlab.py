887755648e5055d1068a833e700f272567f91cfa
Ameenah Burhan <ameenah.burhan@gmail.com>
2024-04-19 03:16:30 -0400

    Adding asset tracking telemetry testing (#5)
    * Update gitlab.py
    
    * Update gitlab.py

--- outer-loop-cli/src/devai/commands/gitlab.py
+++ outer-loop-cli/src/devai/commands/gitlab.py
@@ -18,8 +18,12 @@ from langchain.agents import AgentType, initialize_agent
 from langchain_google_vertexai import ChatVertexAI
 from langchain_community.agent_toolkits.gitlab.toolkit import GitLabToolkit
 from langchain_community.utilities.gitlab import GitLabAPIWrapper
+from google.cloud.aiplatform import telemetry
+  
+USER_AGENT = 'cloud-solutions/genai-outer-loop-devai'
 
-llm = ChatVertexAI(model_name="gemini-pro",
+with telemetry.tool_context_manager(USER_AGENT):
+  llm = ChatVertexAI(model_name="gemini-pro",
                    convert_system_message_to_human=True,
                    project=os.environ["PROJECT_ID"],
                    location=os.environ["LOCATION"])
