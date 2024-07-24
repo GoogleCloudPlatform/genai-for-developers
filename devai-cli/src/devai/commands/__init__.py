94a765511a1caa76f0a99faa9d3ae869f249cf64
Andrey Shakirov <andreyshakirov@google.com>
2024-07-24 15:58:45 -0400

    removed check for Git

--- devai-cli/src/devai/commands/__init__.py
+++ devai-cli/src/devai/commands/__init__.py
@@ -2,8 +2,5 @@ import shutil, os, sys
 
 
 os.environ["GIT_PYTHON_REFRESH"] = "quiet"
-if shutil.which("git") is None:
-    print("Git is required but not installed. Exiting.")
-    sys.exit(1)
 
 from . import cmd, prompt, release, review, document
